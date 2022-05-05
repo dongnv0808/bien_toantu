5bbe5d51cd50d4e0e55b3c76fcca7157c70021d0
dongnv0808 <yamaraja0808@gmail.com>
2022-05-05 16:33:32 +0700

    bien_toantu

---
+++ bien_toantu.js
@@ -0,0 +1,6 @@
+inputWidth = prompt("Enter the Width");
+inputHeight = prompt("Enter the Height");
+let inputWidth1 = parseInt(inputWidth);
+let inputHeight1 = parseInt(inputHeight);
+let area = inputWidth1*inputHeight1;
+document.write("This is area=" + area);
